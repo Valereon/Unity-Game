a1417c91b62f47aa6c683daf54b572e0fdaa1cd0
Valereon <tools418@gmail.com>
2023-09-10 11:10:32 -0400

    update script to make ships not done yet tho

--- Assets/_Scripts/ShipHelm.cs
+++ Assets/_Scripts/ShipHelm.cs
@@ -1,31 +1,44 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
+using UnityEditor.Media;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class ShipHelm : MonoBehaviour
 {
     Vector3 origin;
     GameManager gm;
+    ShipCreator shipCreator;
     GridPlacement gridPlacement;
+
+    PlayerMovement playerMovement;
+
+    int thrusterAmount;
+    bool isOnShip = false;
 
     // Start is called before the first frame update
     void Start()
     {
         gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+        shipCreator = gm.GetComponent<ShipCreator>();
         gridPlacement = gm.GetComponent<GridPlacement>();
+        playerMovement = GameObject.Find("Player").GetComponent<PlayerMovement>();
         origin = transform.position;
 
     }
     
     private void OnMouseOver() {
-        if(Input.GetKeyDown(KeyCode.E)){
+        if(Input.GetKeyDown(KeyCode.E) && shipCreator.isShipMode){
             MakeShip();
 
         }
-        if(Input.GetKeyDown(KeyCode.Q)){
+        else if(Input.GetKeyDown(KeyCode.Q) && shipCreator.isShipMode){
             transform.Rotate(0, 0, -90);
+        }else if (Input.GetKeyDown(KeyCode.E) && !shipCreator.isBuildMode && shipCreator.isShipMode){
+
+            BoardShip();
+            isOnShip = true;
+
         }
     }
 
@@ -50,6 +63,9 @@ public class ShipHelm : MonoBehaviour
         float distance = 0.5f;
         List<Collider2D> colliders = new();
         while(GetColliders(distance).Length > 0){
+            foreach(Collider2D collider in GetColliders(distance)){
+                
+            }
             GetColliders(distance).ToList().ForEach(colliders.Add);
 
             try{
@@ -73,7 +89,30 @@ public class ShipHelm : MonoBehaviour
 
 
     Collider2D[] GetColliders(float distance){
-        return Physics2D.OverlapCircleAll(new Vector3(origin.x + distance, origin.y + distance, origin.z), distance);
+        return Physics2D.OverlapCircleAll(new Vector3(origin.x, origin.y, origin.z), distance);
+    }
+
+
+
+    void BoardShip()
+    {
+        playerMovement.enabled = false;
+        playerMovement.gameObject.transform.SetParent(transform);
+        playerMovement.gameObject.transform.position = transform.position;
+    }
+
+    void FixedUpdate() {
+        if(!isOnShip)
+            return;
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector2 direction = new Vector2(horizontal, vertical).normalized;
+        
+        GetComponent<Rigidbody2D>().AddForce(direction * thrusterAmount * 5f, ForceMode2D.Force);
+
+        if(Input.GetKey(KeyCode.LeftShift)){
+            GetComponent<Rigidbody2D>().AddForce(direction * thrusterAmount * 10f, ForceMode2D.Impulse);
+        }
     }
 }
 
